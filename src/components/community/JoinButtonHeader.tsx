2efb08045a0a74b796ec417cc3c50a61f18cd95b
Alan Junqueira <contato.alanjunqueira@gmail.com>
2023-04-26 11:41:28 -0300

    feat: create leaveCommunity and joinCommunity methods

--- src/components/community/JoinButtonHeader.tsx
+++ src/components/community/JoinButtonHeader.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from '../Button'
 import { useCommunityStore } from '@/store/community/useCommunityStore'
 import { Community } from '@/@types/Community'
@@ -12,10 +12,22 @@ interface IJoinButtonHeader {
 }
 
 export const JoinButtonHeader = ({ communityData }: IJoinButtonHeader) => {
-  const [user, loading, error] = useAuthState(auth)
+  const [user, userLoading, error] = useAuthState(auth)
+  const [toggleCommunityLoading, setToggleCommunityLoading] = useState<boolean>(false);
 
   const { state: { mySnippets }, actions: { onJoinOrLeaveCommunity, getMySnippets, resetSnippets } } = useCommunityStore()
   const isJoined = !!mySnippets.find(item => item.communityId === communityData.id)
+
+  const handleToggleCommunity = () => {
+    try {
+      setToggleCommunityLoading(true)
+      onJoinOrLeaveCommunity(communityData, user, isJoined)
+    } catch (error) {
+      console.log('handleToggleCommunity error', error)
+    } finally {
+      setToggleCommunityLoading(false)
+    }
+  }
 
   useEffect(() => {
     if (!user) {
@@ -28,9 +40,9 @@ export const JoinButtonHeader = ({ communityData }: IJoinButtonHeader) => {
   return (
     <Button
       variant={isJoined ? 'outline' : 'solid'}
-      className='h-6'
-      onClick={() => onJoinOrLeaveCommunity(communityData, isJoined)}
-      disabled={loading}
+      className='h-7'
+      onClick={handleToggleCommunity}
+      disabled={userLoading || toggleCommunityLoading}
     >
       {isJoined ? 'Joined' : 'Join'}
     </Button>
--- src/store/community/useCommunityStore.ts
+++ src/store/community/useCommunityStore.ts
@@ -1,7 +1,7 @@
 import { Community } from '@/@types/Community'
 import { firestore } from '@/firebase/clientApp'
 import { User } from 'firebase/auth'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, doc, getDocs, increment, writeBatch } from 'firebase/firestore'
 import { create } from 'zustand'
 
 type CommunitySnippet = {
@@ -17,9 +17,9 @@ type CommunityState = {
 
 type CommunityActions = {
   getMySnippets: (user: any) => void
-  joinCommunity: (communityData: Community) => void
-  leaveCommunity: (communityId: string) => void
-  onJoinOrLeaveCommunity: (communityData: Community, isJoined: boolean) => void
+  joinCommunity: (communityData: Community, user: User) => void
+  leaveCommunity: (communityId: string, user: User) => void
+  onJoinOrLeaveCommunity: (communityData: Community, user: any, isJoined: boolean) => void
   resetSnippets: () => void
 }
 
@@ -55,16 +55,56 @@ export const useCommunityStore = create<CommunityStoreProps>((set, get, actions)
         state: { ...prev.state, mySnippets: [] },
       }))
     },
-    leaveCommunity: (communityId) => { },
-    joinCommunity: () => { /* implementação da função */ },
-    onJoinOrLeaveCommunity: (communityData, isJoined) => {
+    leaveCommunity: async (communityId: string, user: User) => {
+      try {
+        const batch = writeBatch(firestore)
+        batch.delete(doc(firestore, `users/${user.uid}/communitySnippets`, communityId))
+        batch.update(doc(firestore, 'communities', communityId), {
+          numberOfMembers: increment(-1)
+        })
+
+        await batch.commit()
+
+        set((prev) => ({
+          ...prev,
+          state: { ...prev.state, mySnippets: prev.state.mySnippets.filter(item => item.communityId !== communityId) },
+        }))
+      } catch (error) {
+        console.log('leaveCommunityError', error)
+      }
+    },
+    joinCommunity: async (communityData: Community, user: User) => {
+      try {
+        const batch = writeBatch(firestore)
+        const newSnippet: CommunitySnippet = {
+          communityId: communityData.id,
+          imageUrl: communityData.imageUrl || ''
+        }
+
+        batch.set(doc(firestore, `users/${user.uid}/communitySnippets`, communityData.id), newSnippet)
+        batch.update(doc(firestore, 'communities', communityData.id), {
+          numberOfMembers: increment(1)
+        })
+
+        await batch.commit()
+
+        set((prev) => ({
+          ...prev,
+          state: { ...prev.state, mySnippets: [...prev.state.mySnippets, newSnippet] },
+        }))
+
+      } catch (error) {
+        console.log('joinCommunity error', error)
+      }
+    },
+    onJoinOrLeaveCommunity: (communityData: Community, user: User, isJoined: boolean) => {
       if (isJoined) {
         // agora você pode acessar a função joinCommunity
-        get().actions.leaveCommunity(communityData.id)
+        get().actions.leaveCommunity(communityData.id, user)
         return
       }
 
-      get().actions.joinCommunity(communityData)
+      get().actions.joinCommunity(communityData, user)
     }
   }
 }))
